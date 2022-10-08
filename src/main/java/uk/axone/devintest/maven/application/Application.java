9d650b717383d6fa8b3cb579ba214402d1960571
axoneuk <info@axone-tech.uk>
2022-10-08 08:24:55 +0100

    Committing the project the first time

---
+++ src/main/java/uk/axone/devintest/maven/application/Application.java
@@ -0,0 +1,9 @@
+package uk.axone.devintest.maven.application;
+
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("My application");
+    }
+}
